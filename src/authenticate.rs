5d993dfba5f0b75ab8c5c9f37da9668937124e59
Knut Sebastian Tungland <stunglan@gmail.com>
2021-06-09 16:26:42 +0200

    unpack rbearer token

--- src/authenticate.rs
+++ src/authenticate.rs
@@ -2,6 +2,7 @@ use dotenv::dotenv;
 use std::collections::HashMap;
 use std::env;
 use std::fmt;
+use serde_json::Value;
 
 #[derive(Debug)]
 pub struct ServerConfig {
@@ -24,13 +25,26 @@ fn get_env(key: &str) -> String {
 
 impl fmt::Display for ServerConfig {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-      write!(f, "({}, {}, {}, {}, {},{})", self.user_name, self.user_password,self.application_key,self.auth_uri,self.application_key,self.bearer)
+    write!(
+      f,
+      "({}, {}, {}, {}, {},{})",
+      self.user_name,
+      self.user_password,
+      self.application_key,
+      self.auth_uri,
+      self.application_key,
+      self.bearer
+    )
   }
 }
 
 impl ServerConfig {
-
-  async fn login(user_name: String, user_password: String, application_key: String,auth_uri: String) -> Result<String, Box<dyn std::error::Error>> {
+  async fn login(
+    user_name: &str,
+    user_password: &str,
+    application_key: &str,
+    auth_uri: &str,
+  ) -> String {
     let mut map = HashMap::new();
     map.insert("grant_type", "password");
     map.insert("client_id", &application_key);
@@ -38,13 +52,24 @@ impl ServerConfig {
     map.insert("password", &user_password);
 
     let client = reqwest::Client::new();
-    let resp = client.post(auth_uri).form(&map).send().await?;
+    let resp = client.post(auth_uri).form(&map).send().await;
+    match resp {
+      Ok(resp) => {
 
-    println!("Hello, login {:?}", resp);
-
-    println!("Hello, test!");
-    Ok(resp.text().await?)
-
+        let result = resp.text().await;
+        match result {
+          Ok(value) => {
+            return value;
+          }
+          Err(err) => {
+            panic!("Error in extracting text from what returned from {:}\nErr; {:}",auth_uri, err)
+          }
+        }
+      }
+      Err(err) => {
+        panic!("Error in extracting text from  {:}\nErr; {:}", auth_uri,err)
+      }
+    }
   }
 
   pub async fn new() -> ServerConfig {
@@ -54,20 +79,22 @@ impl ServerConfig {
     let user_password = get_env("USER_PASSWORD");
     let application_key = get_env("APPLICATION_KEY");
     let auth_uri = get_env("AUTH_URI");
+    let app_uri = get_env("AUTH_URI");
 
-    let resp = ServerConfig::login(user_name, user_password, application_key,auth_uri);
+    let resp = ServerConfig::login(&user_name, &user_password, &application_key, &auth_uri);
+    
+    let resp = resp.await;
 
-    println!("Hello, res {:?}", resp.await);
+    println!("Hello, res {:?}", resp);
+    let v: Value = serde_json::from_str(&resp).unwrap();
 
     ServerConfig {
-      user_name: String::from("String"),
-      user_password: String::from("String"),
-      application_key: String::from("String"),
-      auth_uri: String::from("String"),
-      app_uri: String::from("String"),
-      bearer: String::from("String"),
+      user_name: String::from(user_name),
+      user_password: String::from(user_password),
+      application_key: String::from(application_key),
+      auth_uri: String::from(auth_uri),
+      app_uri: String::from(app_uri),
+      bearer: v["access_token"].to_string(),
     }
   }
 }
-
-
